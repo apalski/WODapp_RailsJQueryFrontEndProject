1e5c660d10d41546e217eecaeb79ddfa52b018c7
github username apalski <github email address annette.drapalski@gmail.com>
2018-04-25 11:49:50 +1000

    Remove unused new movement code

--- app/assets/javascripts/movements.js
+++ app/assets/javascripts/movements.js
@@ -7,13 +7,3 @@ $(function(){
  	 e.preventDefault();
   });	 
 });
-
-$(function(){
-	$("a.load_new_movement").on("click", function(e) {
-		$.ajax({
-			url: this.href,
-			dataType: "script"
-		});
-		e.preventDefault();
-	});
-});
