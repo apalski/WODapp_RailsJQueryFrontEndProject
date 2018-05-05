be13b0eb26b1c863962db4a5ba7aacf387259dec
github username apalski <github email address annette.drapalski@gmail.com>
2018-05-05 15:55:47 +1000

    Update redirects to users show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
 			@user = User.find_by(:name => params[:user][:name])
 			if @user && @user.authenticate(params[:user][:password])
 				session[:user_id] = @user.id
-				redirect_to root_path
+				redirect_to user_path(@user)
 			else
 				flash[:notice] = "You couldn't be signed in, please check your name and password and try again"
 				redirect_to sessions_new_path
